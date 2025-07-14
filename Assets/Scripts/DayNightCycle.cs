89eb15cbe81369538ed235ffbd34145eaefe22a8
Shinjiyongyee <shinjiyong0904@gmail.com>
2025-07-14 09:39:07 +0900

    add both rotation and orbit
    자전과 공전을 동시에 구현

--- Assets/Scripts/DayNightCycle.cs
+++ Assets/Scripts/DayNightCycle.cs
@@ -2,10 +2,15 @@ using UnityEngine;
 
 public class DayNightCycle : MonoBehaviour
 {
-    public float sunSpeed = 288f;
+    float sunRotationSpeed = 288f;
+    float sunOrbitSpeed = 30f;
 
     void Update()
     {
-        transform.rotation = Quaternion.Euler(Mathf.Abs((Time.time * sunSpeed) % 180), 30, 0);
+        transform.rotation = Quaternion.Euler(
+            Mathf.Abs((Time.time * sunRotationSpeed) % 180), 
+            (Time.time * sunOrbitSpeed) % 360, 
+            0
+            );
     }
 }
